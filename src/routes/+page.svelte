7462361f2d9d904124db4703af964e2422bdcb39
Justin Forlenza <jforlenza@steamcenter.nyc>
2024-11-25 07:48:36 -0500

    tweaked shrink/expand feature

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -164,36 +164,45 @@ $: encodeCode(value)
 
   <div class="grid-area-[actions] flex items-center justify-center lg:justify-between px-2">
     <button 
-      class="hidden lg:block font-sans px-6 py-1 rounded text-lg bg-slate-50 border-slate-4 border-1 hover:bg-slate-1 text-slate-8"
-      aria-details="expand output to be larger"
+      class="hidden lg:block font-sans px-5 py-2 rounded text-lg bg-slate-50 border-slate-4 border-1 hover:bg-slate-1 text-slate-8"
+      title={`${expanded ? 'Shrink' : 'Expand'} Output`}
+      aria-label={`${expanded ? 'shrink' : 'expand'} code output`}
+      aria-details="toggle expanded code output"
+      aria-pressed={expanded}
       on:click={() => {
         expanded = !expanded
-        setTimeout(windowResize, 100)
+        setTimeout(()=> {
+          windowResize()
+        }, 250)
       }}
     >
-      {expanded ? 'Shrink' : 'Expand'} Output
+      {#if expanded}
+        <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="size-5"><path d="M5 14l7-6.5L5 1" stroke="currentColor" stroke-linecap="square"></path></svg>
+      {:else}
+        <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="size-5"><path d="M10 14L3 7.5 10 1" stroke="currentColor" stroke-linecap="square"></path></svg>
+      {/if}
     </button>
     <div class="flex gap-4">
       <button 
-      class="font-sans px-6 py-1 rounded text-lg bg-green-50 border-green-4 border-1 hover:bg-green-1 text-green-8 relative"
-      class:text-transparent={running}
-      aria-details="run code"
-      on:click={runPython}
-    >
-      {#if running}
-        <span class="hidden" aria-live="polite" aria-busy="true">Working ... </span>
-        <div class="absolute fill-green-9 h-full w-full flex justify-center items-center top-0 left-0">{@html spinner}</div>
-      {/if}
-      Run
-    </button>
+        class="font-sans px-6 py-1 rounded text-lg bg-green-50 border-green-4 border-1 hover:bg-green-1 text-green-8 relative"
+        class:text-transparent={running}
+        aria-details="run code"
+        on:click={runPython}
+      >
+        {#if running}
+          <span class="hidden" aria-live="polite" aria-busy="true">Working ... </span>
+          <div class="absolute fill-green-9 h-full w-full flex justify-center items-center top-0 left-0">{@html spinner}</div>
+        {/if}
+        Run
+      </button>
 
-    <button 
-      class="font-sans px-6 py-1 rounded text-lg bg-blue-50 border-blue-4 border-1 hover:bg-blue-1 text-blue-8"
-      aria-details="copy share link to code repl"
-      on:click={() => {navigator.clipboard.writeText(window.location.toString()); alert('Link Copied')}}
-    >
-      Share
-    </button>
+      <button 
+        class="font-sans px-6 py-1 rounded text-lg bg-blue-50 border-blue-4 border-1 hover:bg-blue-1 text-blue-8"
+        aria-details="copy share link to code repl"
+        on:click={() => {navigator.clipboard.writeText(window.location.toString()); alert('Link Copied')}}
+      >
+        Share
+      </button>
     </div>
 
   </div>
