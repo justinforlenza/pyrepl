851335dfde4a72c25dfac44a4a6379648e68d6a4
Justin <jforlenza@steamcenter.nyc>
2025-03-10 13:30:04 -0400

    refactor editor

---
+++ src/components/Editor.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+import { replaceState } from '$app/navigation'
+import { page } from '$app/state'
+
+import CodeMirror from 'svelte-codemirror-editor'
+import { python } from '@codemirror/lang-python'
+
+import { app } from '$lib/state'
+import { atou, utoa } from '$lib'
+
+
+try {
+  app.value = atou(page.url.searchParams.get('code') ?? '')
+} catch (e) {
+  // @ts-expect-error error type unknown
+  if (e.name === 'InvalidCharacterError') {
+    let [url, hash] = window.location.toString().split('?code=')
+    if (hash !== undefined) {
+      window.location.assign(`${url}?code=${encodeURIComponent(hash)}`)
+    }
+  }
+}
+
+const encodeCode = (code: string) => {
+  if (typeof window === 'undefined' || code === '') return
+
+  const hash = utoa(code)
+
+  if (page.url.searchParams.get('code') !== hash) {
+    replaceState(`?code=${encodeURIComponent(hash)}`, {})
+  }
+}
+
+$: encodeCode(app.value)
+</script>
+
+
+<CodeMirror
+  bind:value={app.value}
+  lang={python()}
+  class="rounded-xl overflow-hidden bg-white transition-all"
+  styles={{
+    '&': {
+      height: '100%',
+      fontSize: '18px'
+    }
+  }}
+/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,12 @@
 <script lang="ts">
-import { replaceState } from '$app/navigation'
-import { page } from '$app/state'
-
-import CodeMirror from 'svelte-codemirror-editor'
-import { python } from '@codemirror/lang-python'
 
 import Terminal from '../components/Terminal.svelte'
 import Actions from '../components/Actions.svelte'
 
 import { app } from '$lib/state'
-import { atou, utoa } from '$lib'
+    import Editor from '../components/Editor.svelte';
 
-try {
-  app.value = atou(page.url.searchParams.get('code') ?? '')
-} catch (e) {
-  // @ts-expect-error error type unknown
-  if (e.name === 'InvalidCharacterError') {
-    let [url, hash] = window.location.toString().split('?code=')
-    if (hash !== undefined) {
-      window.location.assign(`${url}?code=${encodeURIComponent(hash)}`)
-    }
-  }
-}
 
-const encodeCode = (code: string) => {
-  if (typeof window === 'undefined' || code === '') return
-
-  const hash = utoa(code)
-
-  if (page.url.searchParams.get('code') !== hash) {
-    replaceState(`?code=${encodeURIComponent(hash)}`, {})
-  }
-}
-
-$: encodeCode(app.value)
 </script>
 
 <svelte:window onresize={app.resize} />
@@ -67,17 +40,7 @@ $: encodeCode(app.value)
 
   <Actions />
 
-  <CodeMirror
-    bind:value={app.value}
-    lang={python()}
-    class="rounded-xl overflow-hidden bg-white transition-all"
-    styles={{
-      '&': {
-        height: '100%',
-        fontSize: '18px'
-      }
-    }}
-  />
+  <Editor />
 
   <Terminal />
 </main>
